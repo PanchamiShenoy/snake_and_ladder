681b527cdc0db4f85e6d949b3d96f7ee198acde1
panchami shenoy <panchamishenoy16@gmail.com>
2021-08-23 22:13:32 +0530

    game start at position 0

--- src/com/snake/SnakeAndLadder.java
+++ src/com/snake/SnakeAndLadder.java
@@ -5,6 +5,9 @@ public class SnakeAndLadder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Snake and Ladder Program");
+		
+		int position=0;
+		System.out.println("Single player is at postion "+position);
 	}
 
 }
